ccb8dabd868458e2d3f71cb02d569de6653bcfbd
sandrasansand <sandrasaezcarbonell@gmail.com>
2023-01-06 07:37:28 +0100

    Ts
    Ej básicos para iniciarse en Ts

---
+++ 001/funciones.js
@@ -0,0 +1,16 @@
+"use strict";
+//iniciado en string es string
+let saludo = "hola, probando ts";
+//:number
+let num = 10;
+//tipo dato :any cualquier tipo de dato
+let cualquiera;
+cualquiera = "bueno";
+const PI_NUM = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(num);
+    console.log(cualquiera);
+    console.log(PI_NUM);
+}
+saludar();
---
+++ 003_interfaces/interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+let usuario1 = { nombreUsuario: "Lucas", password: "1234" };
+console.log(usuario1);
+console.log(usuario1.nombreUsuario);
+let avion = {
+    abordarTransporte: function () {
+        console.log("Preparado para despegar");
+    }
+};
+avion.abordarTransporte();
